a5ecd0c44a93987f70cbc45ef3a93a8d7044a212
Mike Villalard <michael@mvi-design.co.uk>
2024-03-04 09:52:17 +0000

    Updated logo to adapt to colorMode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,15 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
-import logo from "../assets/images/logo/logo_dark.png";
+import { HStack, Image, Text, useColorMode } from "@chakra-ui/react";
+import logo_dark from "../assets/images/logo/logo_dark.png";
+import logo_light from "../assets/images/logo/logo_light.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 function NavBar() {
+  const { colorMode } = useColorMode();
+  const logoImage = colorMode === "dark" ? logo_dark : logo_light;
+
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} width="100px" />
+      <Image src={logoImage} width="100px" />
       <ColorModeSwitch />
     </HStack>
   );
