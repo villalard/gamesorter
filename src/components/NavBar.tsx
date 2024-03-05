a727df6f5d62c23a5cbcafa886b77a72b3bb575c
Mike Villalard <michael@mvi-design.co.uk>
2024-03-05 21:39:07 +0000

    Impliment search

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,14 +4,18 @@ import logo_light from "../assets/images/logo/logo_light.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 
-function NavBar() {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+function NavBar({ onSearch }: Props) {
   const { colorMode } = useColorMode();
   const logoImage = colorMode === "dark" ? logo_dark : logo_light;
 
   return (
     <HStack padding="10px">
       <Image src={logoImage} width="100px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
