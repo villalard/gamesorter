618d110de265072ddd91be4d14d8db112a83fe1a
Mike Villalard <michael@mvi-design.co.uk>
2024-03-11 16:08:28 +0000

    Added RAWG API KEY to environment

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '731c0fc003554dfc9e78d20b7791d531'
+        key: process.env.VITE_RAWG_API_KEY
     }
 });
